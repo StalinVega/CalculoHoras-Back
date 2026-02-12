e49bbb3b3250f610efe7da21ef262faa793c7d4a
Stalin Vega <stalinvega18@gmail.com>
2026-02-12 10:40:12 -0500

    INICIO

---
+++ src/main/java/com/litolaser/calculoHoras/CalculoHorasApplication.java
@@ -0,0 +1,13 @@
+package com.litolaser.calculoHoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/litolaser/calculoHoras/CalculoHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litolaser.calculoHoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
